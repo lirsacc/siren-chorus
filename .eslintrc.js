87b13ea20bbf1542a503029ea9ecaccfde3827bb
Charles Lirsac <code@lirsac.com>
2023-11-24 12:18:38 +0000

    Add eslint react plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
   extends: [
     "prettier",
     "eslint:recommended",
@@ -27,6 +27,10 @@ module.exports = {
   settings: {
     react: {
       pragma: "h",
+      react: {
+        pragma: "h",
+        version: "16.0",
+      },
     },
     "import/resolver": {
       typescript: true,
@@ -61,5 +65,37 @@ module.exports = {
         pathGroupsExcludedImportTypes: ["preact", "preact/*"],
       },
     ],
+    "react/no-deprecated": 2,
+    "react/react-in-jsx-scope": 0, // handled this automatically
+    "react/display-name": [1, { ignoreTranspilerName: false }],
+    "react/jsx-no-bind": [
+      1,
+      {
+        ignoreRefs: true,
+        allowFunctions: true,
+        allowArrowFunctions: true,
+      },
+    ],
+    "react/jsx-no-comment-textnodes": 2,
+    "react/jsx-no-duplicate-props": 2,
+    "react/jsx-no-target-blank": 2,
+    "react/jsx-no-undef": 2,
+    "react/jsx-tag-spacing": [2, { beforeSelfClosing: "always" }],
+    "react/jsx-uses-react": 2, // debatable
+    "react/jsx-uses-vars": 2,
+    "react/jsx-key": [2, { checkFragmentShorthand: true }],
+    "react/self-closing-comp": 2,
+    "react/prefer-es6-class": 2,
+    "react/prefer-stateless-function": 1,
+    "react/require-render-return": 2,
+    "react/no-danger": 1,
+    // Legacy APIs not supported in Preact
+    "react/no-did-mount-set-state": 2,
+    "react/no-did-update-set-state": 2,
+    "react/no-find-dom-node": 2,
+    "react/no-is-mounted": 2,
+    "react/no-string-refs": 2,
+    "react-hooks/rules-of-hooks": 2,
+    "react-hooks/exhaustive-deps": 1,
   },
 };
