03806e2b1afd48ea32a2472f68a2ada347990ab9
Charles Lirsac <code@lirsac.com>
2023-11-27 21:41:56 +0000

    Minor display adjustments

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,18 @@
-import { useState, useEffect } from "preact/hooks";
+import { useState, useEffect, useMemo } from "preact/hooks";
+
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  animals,
+} from "unique-names-generator";
+
+export function randomName(): string {
+  return uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    separator: "-",
+    length: 2,
+  });
+}
 
 export function randomIshId(length: number = 20): string {
   return Array.from(Array(length), () =>
@@ -16,43 +30,39 @@ export function useFlag(initial: boolean) {
   };
 }
 
+const STORAGE_PREFIX = "siren-chorus:1:";
+
 export function useStoredString(
   key: string,
   fallback: string,
 ): [string, (value: string) => void] {
-  const stored = localStorage.getItem(key);
+  const prefixedKey = useMemo(() => `${STORAGE_PREFIX}${key}`, [key]);
+  const stored = localStorage.getItem(prefixedKey);
   const [value, setValue] = useState(stored != null ? stored : fallback);
 
   useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
+    localStorage.setItem(prefixedKey, value);
+  }, [value, prefixedKey]);
 
   useEffect(() => {
-    const stored = localStorage.getItem(key);
+    const stored = localStorage.getItem(prefixedKey);
     if (stored != null) {
       setValue(stored);
     }
-  }, [key]);
+  }, [prefixedKey]);
 
   return [value, setValue];
 }
 
-const USER_COLOURS = [
-  { bg: "#0a93af", text: "#000000" },
-  { bg: "#1663d0", text: "#ffffff" },
-  { bg: "#3945da", text: "#ffffff" },
-  { bg: "#703bd7", text: "#ffffff" },
-  { bg: "#b13bc8", text: "#ffffff" },
-  { bg: "#ffe154", text: "#000000" },
-  { bg: "#c7ea63", text: "#000000" },
-  { bg: "#88e198", text: "#000000" },
-  { bg: "#52dba3", text: "#000000" },
-  { bg: "#55e0e1", text: "#000000" },
-  { bg: "#4500ff", text: "#ffffff" },
-  { bg: "#bd03f4", text: "#ffffff" },
-  { bg: "#fc087b", text: "#000000" },
-  { bg: "#ff0f08", text: "#000000" },
-  { bg: "#ff7000", text: "#000000" },
+export const USER_COLOURS = [
+  { color: "#30bced", light: "#30bced33" },
+  { color: "#6eeb83", light: "#6eeb8333" },
+  { color: "#ffbc42", light: "#ffbc4233" },
+  { color: "#ecd444", light: "#ecd44433" },
+  { color: "#ee6352", light: "#ee635233" },
+  { color: "#9ac2c9", light: "#9ac2c933" },
+  { color: "#8acb88", light: "#8acb8833" },
+  { color: "#1be7ff", light: "#1be7ff33" },
 ];
 
 export function getRandomColour() {
