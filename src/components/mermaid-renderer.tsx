15bfc0c1e95e2f42d272889c27792e70b7c73aec
Charles Lirsac <code@lirsac.com>
2023-11-28 18:50:52 +0000

    (wip) align contro;

--- src/components/mermaid-renderer.tsx
+++ src/components/mermaid-renderer.tsx
@@ -118,6 +118,51 @@ const MermaidRenderer = ({
   const zoomOut = _controlHandler((x) => x.zoomOut());
   const resetZoom = _controlHandler((x) => x.reset({ animate: false }));
 
+  const alignVertically = _controlHandler((x) => {
+    const node = containerRef.current;
+    const parent = containerRef.current?.parentElement;
+
+    if (!node || !parent) return;
+
+    // Try and align so the diagram fits vertically in the available container
+    // and positionned at the top left corner of the container.
+
+    const parentBounds = parent.getBoundingClientRect();
+    const childBounds = node.getBoundingClientRect();
+    const originalScale = x.getScale();
+
+    const originalHeight = childBounds.height / originalScale;
+    const originalWidth = childBounds.width / originalScale;
+
+    // We first get the target scale from the current size so the final height
+    // is the container's height.
+    const targetHeight = parentBounds.height;
+    const targetScale = (originalScale * targetHeight) / childBounds.height;
+
+    // If we just pan(0, 0) the svg will be off center once zoomed in as it will
+    // be centered on its original position with bleeding edges.
+    //
+    // There's a shortcut here that due to the way we display the svg we know
+    // it's always going to fit horinzontally and this grow in height for the
+    // current alignment target.
+    // TODO: Check that ^
+
+    const targetX =
+      -(parentBounds.width - originalWidth * targetScale) / 2 / targetScale;
+    const targetY =
+      (parentBounds.height - originalHeight * targetScale) / 2 / targetScale;
+
+    console.debug(originalWidth, originalHeight, originalScale);
+    console.debug(targetX, targetY, targetScale);
+
+    x.zoom(targetScale);
+
+    x.pan(0, 0);
+
+    const data = { x, parentBounds, childBounds };
+    console.debug(1, data);
+  });
+
   return (
     <div className="h-100 w-100 position-relative">
       <div className="h-100 w-100">
@@ -130,6 +175,7 @@ const MermaidRenderer = ({
       {enableZoom && showZoomControls && (
         <div className="position-absolute end-0 bottom-0 m-1">
           <div class="btn-group m-1">
+            <Control onClick={alignVertically}>Align</Control>
             <Control onClick={resetZoom}>Reset</Control>
           </div>
           <div class="btn-group m-1">
